e399da376637b729e0da7cd450c5476847ae1384
sigifredochacon <24.sigi@estudiantec.cr>
2023-10-11 17:24:39 -0600

    Actualizacion hasta Video 19 Sigifredo

--- SistemaInventario/Areas/Inventario/Controllers/HomeController.cs
+++ SistemaInventario/Areas/Inventario/Controllers/HomeController.cs
@@ -2,8 +2,9 @@
 using SistemaInventario.Modelo.ViewModels;
 using System.Diagnostics;
 
-namespace SistemaInventario.Controllers
+namespace SistemaInventario.Areas.Inventario.Controllers
 {
+    [Area("Inventario")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
